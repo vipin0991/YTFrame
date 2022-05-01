8fbe06e3d3330c32265ff86d545880aebf2ab4ea
Vipin <vipin.dhiman21@gmail.com>
2022-05-01 16:05:22 +0530

    add

--- YTFrame/src/org/testing/TestScripts/TC1.java
+++ YTFrame/src/org/testing/TestScripts/TC1.java
@@ -1,16 +1,25 @@
 package org.testing.TestScripts;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TC1 {
+	
+	ChromeDriver driver;
+	
   @BeforeMethod
   public void method1(){
+	  System.setProperty("webdriver.chrome.driver", "../YTFrame/chromedriver.exe");
+	  driver = new ChromeDriver();
+	  driver.get("https://www.youtube.com/");
+	  driver.manage().window().maximize();
   }
   
   @Test
   public void method2(){
+	  
   }
 
   @AfterMethod
