a0abf0a5551b34c16e4bc909fa35456c454d60d6
Vipin <vipin.dhiman21@gmail.com>
2022-05-01 07:56:33 +0530

    update

--- YTFrame/src/org/testing/TestScripts/TC1.java
+++ YTFrame/src/org/testing/TestScripts/TC1.java
@@ -1,12 +1,20 @@
 package org.testing.TestScripts;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class TC1 {
   @BeforeMethod
   public void method1(){
   }
   
-  @After
+  @Test
   public void method2(){
   }
 
+  @AfterMethod
+  public void method3() {
+	  
+  }
 }
