c93a3c63c76ce115be4d9698b24dfabfa51cfc35
Vipin <vipin.dhiman21@gmail.com>
2022-05-01 17:04:51 +0530

    add and update

--- YTFrame/src/org/testing/TestScripts/TC1.java
+++ YTFrame/src/org/testing/TestScripts/TC1.java
@@ -1,5 +1,6 @@
 package org.testing.TestScripts;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -18,12 +19,30 @@ public class TC1 {
   }
   
   @Test
-  public void method2(){
+  public void method2() throws InterruptedException{
+	  driver.findElement(By.xpath("//yt-formatted-string[text()='Sign in']")).click();
+	  Thread.sleep(1000);
+	  driver.findElement(By.xpath("//input[@type = 'email']")).sendKeys("testingdata98720");
+	  Thread.sleep(1000);
+	  driver.findElement(By.xpath("//span[text()='Next']")).click();
+	  Thread.sleep(3000);
+	  driver.findElement(By.xpath("//input[@aria-label='Enter your password']")).sendKeys("Test209@)(");
+	  Thread.sleep(2000);
+	  driver.findElement(By.xpath("//span[text()='Next']")).click();
+	  Thread.sleep(2000);
+	  driver.findElement(By.xpath("//yt-formatted-string[text()='Explore']")).click();
+	  Thread.sleep(2000);
+	  driver.findElement(By.xpath("//yt-formatted-string[text()='Trending']")).click();
+	  Thread.sleep(2000);
+	  driver.findElement(By.xpath("//img[@alt = 'Avatar image']")).click();
+	  Thread.sleep(2000);
+	  driver.findElement(By.xpath("//yt-formatted-string[text()='Sign out']")).click();
 	  
   }
 
   @AfterMethod
-  public void method3() {
-	  
+  public void method3() throws InterruptedException {
+	  Thread.sleep(10000);
+	  driver.close();
   }
 }
